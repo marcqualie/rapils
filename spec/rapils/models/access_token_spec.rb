3b9f4638e06e1d88142330dffbd8c16a54545710
Marc Qualie <marc@marcqualie.com>
2020-07-25 13:21:25 +0100

    Port over model specs

--- spec/rapils/models/access_token_spec.rb
+++ spec/rapils/models/access_token_spec.rb
@@ -1,4 +1,33 @@
 describe Rapils::Models::AccessToken do
   it { expect(described_class).to be < Rapils::Models::BaseRecord }
   it { expect(described_class.abstract_class).to eq true }
+
+  describe '#expired?' do
+    it 'returns true when expired' do
+      access_token = create(:access_token, expires_at: 1.minute.ago)
+      expect(access_token.expired?).to eq true
+    end
+
+    it 'returns false when expires in future' do
+      access_token = create(:access_token, expires_at: 1.minute.from_now)
+      expect(access_token.expired?).to eq false
+    end
+
+    it 'returns false when no expiry' do
+      access_token = create(:access_token, expires_at: nil)
+      expect(access_token.expired?).to eq false
+    end
+  end
+
+  describe '#invalidated?' do
+    it 'returns true when expired' do
+      access_token = create(:access_token, invalidated_at: 1.minute.ago)
+      expect(access_token.invalidated?).to eq true
+    end
+
+    it 'returns false when no expiry' do
+      access_token = create(:access_token, invalidated_at: nil)
+      expect(access_token.invalidated?).to eq false
+    end
+  end
 end
--- spec/rapils/models/user_spec.rb
+++ spec/rapils/models/user_spec.rb
@@ -1,4 +1,47 @@
 describe Rapils::Models::User do
   it { expect(described_class).to be < Rapils::Models::BaseRecord }
   it { expect(described_class.abstract_class).to eq true }
+
+  describe '#admin?' do
+    it 'returns false when nil' do
+      user = create(:user, admin_roles: nil)
+      expect(user.admin?).to eq false
+    end
+
+    it 'returns false when empty array' do
+      user = create(:user, admin_roles: [])
+      expect(user.admin?).to eq false
+    end
+
+    it 'returns false when a splat array' do
+      user = create(:user, admin_roles: ['*'])
+      expect(user.admin?).to eq true
+    end
+  end
+
+  describe '#access_granted?' do
+    it 'returns false when nil' do
+      user = create(:user, access_granted_at: nil)
+      expect(user.access_granted?).to eq false
+    end
+
+    it 'returns false when date is in the future' do
+      user = create(:user, access_granted_at: 1.day.from_now)
+      expect(user.access_granted?).to eq false
+    end
+
+    it 'returns true when date is in the past' do
+      user = create(:user, access_granted_at: 1.day.ago)
+      expect(user.access_granted?).to eq true
+    end
+  end
+
+  describe '#grant_access!' do
+    it 'sets access granted timestamps' do
+      user = create(:user, access_granted_at: nil)
+      user.grant_access!
+
+      expect(user.access_granted_at).to_not eq nil
+    end
+  end
 end
