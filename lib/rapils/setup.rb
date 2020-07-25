d3d5cb4bbfbf4b6fc3302cffb1f4f499ce39e9a6
Marc Qualie <marc@marcqualie.com>
2020-07-25 16:20:11 +0100

    Remove the need for rails/pg in the app Gemfile

--- lib/rapils/setup.rb
+++ lib/rapils/setup.rb
@@ -15,6 +15,10 @@ module Rapils
   ].freeze
 
   def self.setup(dependencies = DEFAULT_DEPENDENCIES)
+    # Require core libraries that would usually be in app Gemfile
+    require 'diffcrypt'
+    require 'pg'
+
     # Load core rails dependencies
     require 'rails'
     dependencies.each { |dependency| require dependency }
